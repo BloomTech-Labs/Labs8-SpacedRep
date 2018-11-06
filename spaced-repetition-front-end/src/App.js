6faaf1e8e8e26f1ed36c787ff6c6d7365e5a462d
gooseandmegander <meganawilliamson@gmail.com>
2018-11-06 09:08:40 -0800

    Logs in and redirects a user
    Routes other than the landing page are secured wih SecureRoute. If a user is not logged in and tries to access a page requiring authentication, they are redirected to the /login route. Once a user is authenticated, they are redirected to their user dashboard
    	modified:   spaced-repetition-front-end/package.json
    	modified:   spaced-repetition-front-end/src/App.js
    	new file:   spaced-repetition-front-end/src/components/auth/Login.jsx
    	new file:   spaced-repetition-front-end/src/components/auth/LoginForm.jsx
    	modified:   spaced-repetition-front-end/yarn.lock

--- spaced-repetition-front-end/src/App.js
+++ spaced-repetition-front-end/src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import { Route, withRouter, Switch } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import config from './components/auth/config';
+import Login from './components/auth/Login';
 import Wrapper from './components/Wrapper';
 import LandingPage from './components/LandingPage';
 import './App.css';
 
+function onAuthRequired({ history }) {
+  history.push('/login');
+}
+
 class App extends Component {
-  state = {
-
-  }
-
-  componentDidMount() {
-
-  }
-
   render() {
     return (
-      <div className="container">
-        <Route exact path='/' component={Wrapper}/>
-        <Route path='/login' component={LandingPage}/>
-      </div>
+      <Security
+        {...config}
+        onAuthRequired={onAuthRequired}
+      >
+        <div className="container">
+          <Route path='/landing' component={LandingPage} />
+          <Route path='/login' render={() => <Login baseUrl={config.url} />} />
+          <Route path='/implicit/callback' component={ImplicitCallback} />
+          <SecureRoute exact path='/' component={Wrapper} />
+        </div>
+      </Security>
     );
   }
 }
