4f1ecb79f39605c6b0282cda77c17b6abe5bb405
Gabriel Duquette <gabrielduquette@gmail.com>
2018-11-05 20:13:03 -0500

    add create react app

---
+++ spaced-repetition-front-end/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hello world
+      </div>
+    );
+  }
+}
+
+export default App;
