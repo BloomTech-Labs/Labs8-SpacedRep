a24cb6483fda3fb6e96c5fd9fcfac44f6b23f723
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-11-07 02:29:03 -0800

    removed unnecessary knex statement

--- spaced-repetition-back-end/db/migrations/usersMigration.js
+++ spaced-repetition-back-end/db/migrations/usersMigration.js
@@ -10,7 +10,3 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   return knex.schema.dropTableIfExists('users');
 };
-
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('Users');
-};
