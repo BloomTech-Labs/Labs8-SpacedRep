00009457a11842b7fba86ccf0c7df602fd1b6971
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-11-07 13:39:33 -0800

    remove gitignore build for netlify testing autodeploy

---
+++ spaced-repetition-front-end/build/precache-manifest.f8ba2eea68541eea469591a6f56cdde0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca105bff28cdfa33c2e5",
+    "url": "/static/js/main.ca105bff.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "ca105bff28cdfa33c2e5",
+    "url": "/static/css/main.2dcfc5f9.chunk.css"
+  },
+  {
+    "revision": "87009c196589fbe442879f8c5bbabdd6",
+    "url": "/index.html"
+  }
+];
