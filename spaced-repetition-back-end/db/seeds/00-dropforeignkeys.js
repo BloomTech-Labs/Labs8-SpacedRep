d15fc8acd8c757336e41ac1636fa4d14936fbcfc
gooseandmegander <meganawilliamson@gmail.com>
2018-11-08 15:49:45 -0800

    Adds user-deck junction table to database
    Adds migration and seeds. Follows protocol to drop the foreign constraint, truncate, and then re-add the foreign constraint after.
     Changes to be committed:
    	renamed:    db/20181108144527_userDeck.js -> db/migrations/20181108144527_userDeck.js
    	modified:   db/seeds/00-dropforeignkeys.js
    	renamed:    db/03-userDeck.js -> db/seeds/03-userDeck.js

--- spaced-repetition-back-end/db/seeds/00-dropforeignkeys.js
+++ spaced-repetition-back-end/db/seeds/00-dropforeignkeys.js
@@ -4,14 +4,24 @@
 
 // NOTE: commented out truncate on seeds!
 
-exports.seed = async function(knex, Promise) {
-  await knex.schema.table('decks', function(tbl) {
-      tbl.dropForeign('author');
-  });
-  await knex('users').truncate();
-  await knex('decks').truncate();
-  await knex.schema.table('decks', function(tbl) {
-      // tbl.foreign('author').references('id').inTable('users').onDelete('cascade');
-      tbl.foreign('author').references('users.id');
-  });
+exports.seed = async function (knex, Promise) {
+    await knex.schema.table('decks', function (tbl) {
+        tbl.dropForeign('author');
+    });
+    await knex.schema.table('userDeck', function (tbl) {
+        tbl.dropForeign('user_id');
+        tbl.dropForeign('deck_id');
+    });
+    await knex('users').truncate();
+    await knex('decks').truncate();
+    await knex('userDeck').truncate();
+    await knex.schema.table('decks', function (tbl) {
+        // tbl.foreign('author').references('id').inTable('users').onDelete('cascade');
+        tbl.foreign('author').references('users.id');
+    });
+    await knex.schema.table('userDeck', function (tbl) {
+        // tbl.foreign('author').references('id').inTable('users').onDelete('cascade');
+        tbl.foreign('user_id').references('users.id');
+        tbl.foreign('deck_id').references('decks.id');
+    });
 };
