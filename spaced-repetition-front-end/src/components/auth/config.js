3373c1bcfad122166e31b84fd921753ca405816c
gooseandmegander <meganawilliamson@gmail.com>
2018-11-08 22:19:22 -0800

    Makes redirect_uri dynamic again

--- spaced-repetition-front-end/src/components/auth/config.js
+++ spaced-repetition-front-end/src/components/auth/config.js
@@ -2,7 +2,7 @@ const config = {
   url: 'https://dev-714580.oktapreview.com',
   issuer: 'https://dev-714580.oktapreview.com/oauth2/default',
   client_id: '0oahcok82vwwvkL9N0h7',
-  redirect_uri: 'https://srsly-auth.netlify.com/implicit/callback',
+  redirect_uri: window.location.origin + '/implicit/callback',
 }
 
 export default config;
