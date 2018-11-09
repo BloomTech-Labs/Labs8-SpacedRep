f2bcbaa37643a17539a1f8b5fd628615dec0f6b9
gooseandmegander <meganawilliamson@gmail.com>
2018-11-08 20:07:47 -0800

    Adds new client_id relevant to its respective Okta application

--- spaced-repetition-front-end/src/components/auth/config.js
+++ spaced-repetition-front-end/src/components/auth/config.js
@@ -1,7 +1,7 @@
 const config = {
   url: 'https://dev-714580.oktapreview.com',
   issuer: 'https://dev-714580.oktapreview.com/oauth2/default',
-  client_id: '0oah98wfdfTYVh9bu0h7',
+  client_id: '0oahcok82vwwvkL9N0h7',
   redirect_uri: window.location.origin + '/implicit/callback',
 }
 
