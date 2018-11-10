56bf2a54a347768e9facfa296e357c7ce5869577
gooseandmegander <meganawilliamson@gmail.com>
2018-11-10 12:37:43 -0800

    Removes Okta code and code implemented to get Okta up and running
     Changes to be committed:
    	modified:   netlify.toml
    	deleted:    spaced-repetition-back-end/lib/config.jsx
    	modified:   spaced-repetition-back-end/package.json
    	modified:   spaced-repetition-back-end/users/usersRoutes.js
    	modified:   spaced-repetition-back-end/yarn.lock
    	modified:   spaced-repetition-front-end/.gitignore
    	modified:   spaced-repetition-front-end/package.json
    	deleted:    spaced-repetition-front-end/public/_headers
    	modified:   spaced-repetition-front-end/src/App.js
    	deleted:    spaced-repetition-front-end/src/components/auth/Login.js
    	deleted:    spaced-repetition-front-end/src/components/auth/LoginForm.js
    	deleted:    spaced-repetition-front-end/src/components/auth/RegistrationForm.js
    	deleted:    spaced-repetition-front-end/src/components/auth/config.js
    	modified:   spaced-repetition-front-end/yarn.lock

--- spaced-repetition-back-end/lib/config.jsx
+++
@@ -1,8 +0,0 @@
-const okta = require('@okta/okta-sdk-nodejs');
-
-const client = new okta.Client({
-    orgUrl: 'https://dev-714580.oktapreview.com',
-    token: '00UrrMsM_WcMv4YPLfOqqwv4z3_IuH8zjPvlUlRmvX'
-});
-
-module.exports = client;
