7a67c4b03b85ef65ce8c31b1d2d8280125dcc21b
gooseandmegander <meganawilliamson@gmail.com>
2018-11-14 10:09:55 -0800

    Navigates between landing page and dashboard views
    Fixes re-rendering issue by adding history prop to Router in routes file. To use history prop, removed BrowserRouter. Reinstates Switch - now Header is outside of Switch and other routes are inside using exact path.
    
     Changes to be committed:
    	modified:   spaced-repetition-front-end/src/components/LandingPage.js
    	modified:   spaced-repetition-front-end/src/routes.js

--- spaced-repetition-front-end/src/routes.js
+++ spaced-repetition-front-end/src/routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, Router, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Callback from './auth/Callback';
 import Wrapper from './components/Wrapper';
 import LandingPage from './components/LandingPage';
 import Auth from './auth/Auth';
+import history from './history';
 
 const auth = new Auth();
 
@@ -18,20 +19,20 @@ const makeMainRoutes = () => {
   // took history={history} off Router
   return (
     <div>
-      <Router>
+      <Router history={history}>
         <React.Fragment>
-          {/* <Switch> */}
           <Route path="/" render={props => <Header auth={auth} {...props} />} />
-          {/* <Route path="/" render={props => <LandingPage auth={auth} {...props} />} /> */}
-          <Route path="/dashboard" render={props => <Wrapper auth={auth} {...props} />} />
-          <Route
-            path="/callback"
-            render={(props) => {
-              handleAuthentication(props);
-              return <Callback {...props} />;
-            }}
-          />
-          {/* </Switch> */}
+          <Switch>
+            <Route exact path="/" render={props => <LandingPage auth={auth} {...props} />} />
+            <Route exact path="/dashboard" render={props => <Wrapper auth={auth} {...props} />} />
+            <Route
+              path="/callback"
+              render={(props) => {
+                handleAuthentication(props);
+                return <Callback {...props} />;
+              }}
+            />
+          </Switch>
         </React.Fragment>
       </Router>
     </div>
