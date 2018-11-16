1aa2ba284d2665e82dd05477e2710369c5a26dab
Drew Moody <drew.moody@me.com>
2018-11-16 01:34:22 -0500

    lots of cleanup

--- spaced-repetition-back-end/db/migrations/20181108160532_cards.js
+++ spaced-repetition-back-end/db/migrations/20181108160532_cards.js
@@ -1,14 +1,10 @@
-// Omitting codeSnippet for now until more research on the library is done.
-// Tags omitted for same reason.
-// CodeSnippet is omitted for now until formatting is decided.
-
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('cards', tbl => {
         tbl.increments('id').primary();
         tbl.string('title', 255).notNullable();
         tbl.string('question', 255).notNullable();
         tbl.string('answer', 255).notNullable();
-        tbl.string('language', 255).notNullable(); // Need domain constraint
+        tbl.string('language', 255).notNullable(); // domain constraint?
         tbl.integer('deck_id').notNullable().references('id').inTable('decks').onDelete('cascade');
     });
 };
--- spaced-repetition-back-end/db/seeds/02-decks.js
+++ spaced-repetition-back-end/db/seeds/02-decks.js
@@ -1,15 +1,9 @@
 exports.seed = function (knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('decks')
-    // .truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex('decks').insert([
-        { name: 'SQL', public: false, author: 2 },
-        { name: 'React', public: false, author: 2 },
-        { name: 'ES6', public: false, author: 1 },
-        { name: 'Chemistry', public: false, author: 1 },
-        { name: 'Chemistry', public: true, author: 3 }, // Testing second entry with same name
-      ]);
-    });
+  return knex('decks').insert([
+    { name: 'SQL', public: false, author: '2a', tags: 'tag1,tag2,tag3' },
+    { name: 'React', public: false, author: '2a', tags: 'tag1,tag2,tag3' },
+    { name: 'ES6', public: false, author: '1a', tags: 'tag1,tag2,tag3' },
+    { name: 'Chemistry', public: false, author: '1a', tags: 'tag1,tag2,tag3' },
+    { name: 'Chemistry', public: true, author: '3a', tags: 'tag1,tag2,tag3' },
+  ]);
 };
