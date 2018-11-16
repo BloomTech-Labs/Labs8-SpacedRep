df98abe94d640766a858ed231c412ed6ecbb56e1
Gabriel Duquette <gabrielduquette@gmail.com>
2018-11-16 00:27:42 -0500

    add handleCreateNewDeck to DeckList

--- spaced-repetition-back-end/index.js
+++ spaced-repetition-back-end/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const helmet = require('helmet');
 const PORT = process.env.PORT || 4242;
 const cors = require('cors');
-const usersRoutes = require('./users/usersRoutes');
 const stripeRoutes = require('./stripe/stripeRoutes');
 const usersRoutes = require('./routes/users/usersRoutes');
 const decksRoutes = require('./routes/decks/decksRoutes');
