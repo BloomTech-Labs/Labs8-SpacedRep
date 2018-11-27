7f7abb3695fc6f61cf0abdaa7e5c9ebfd59739a7
gooseandmegander <meganawilliamson@gmail.com>
2018-11-27 01:50:08 -0800

    training mode cards are modals

--- spaced-repetition-front-end/src/components/Card.js
+++ spaced-repetition-front-end/src/components/Card.js
@@ -57,65 +57,69 @@ class Card extends React.Component {
     if (redirect) return <Redirect to="/dashboard/decks" />;
     return (
       data ? (
-        <CardContainer>
-          <CardData>
-            <CardTitle>{data.cards[currentCard].title}</CardTitle>
-            <CardText>
-              Question:
-              {' '}
-              {data.cards[currentCard].question}
-            </CardText>
-          </CardData>
-          {trained && (
-            <CardInteractions>
-              <AnimateOnReveal>
-                <CardText>
-                  Answer:
-                  {' '}
-                  {data.cards[currentCard].answer}
-                </CardText>
-                {/* Missed It and Got It buttons should connect to the SRS algorithm */}
-                <CardButton type="button" onClick={this.handleMissed}>Missed It</CardButton>
-                <CardButton type="button" onClick={this.handleGot}>Got It</CardButton>
-                {(currentCard + 1) !== data.cards.length
-                  ? (
-                    <NextCardButton type="button" onClick={this.nextCard} showNext={showNext}>Next</NextCardButton>
-                  )
-                  : (
-                    // Routing users back to deckl ist for now. Could add intermediary
-                    // modal with further options (e.g. train again, deck list, dashboard, etc)
-                    // showNext to string is recommended fix to console warning
-                    <NextCardLink to="/dashboard/decks" shownext={showNext.toString()}>End Training Session</NextCardLink>
-                  )
-                }
-              </AnimateOnReveal>
-              <NextCardProgressText hidePrompt={showNext}>
-                How did you do? Select to see the next card.
-              </NextCardProgressText>
-            </CardInteractions>
-          )}
-          {!trained && (
-            <CardInteractions>
-              <CardButton type="button" onClick={this.showAnswer}>Show Answer</CardButton>
-            </CardInteractions>
-          )}
-          <ProgressText>
-            Progress:
-            {currentCard}
-            /
-            {data.cards.length}
-          </ProgressText>
-          {/* Toggles more options for UX: edit card, quit current training, etc */}
-          {/* Could be modal? */}
-          <OptionsButton type="button" onClick={this.toggleOption}>...</OptionsButton>
-          <OptionsMenu status={showOptions}>
-            <OptionItem
-              onClick={this.quitTrainingSession}
-            >
-              Quit current training session.
-            </OptionItem>
-          </OptionsMenu>
-        </CardContainer>
+        <MainCardContainer>
+          <CardModal>
+            <CardContainer>
+              <CardTitle>{data.cards[currentCard].title}</CardTitle>
+              <CardText>
+                Question:
+                {' '}
+                {data.cards[currentCard].question}
+              </CardText>
+            </CardContainer>
+            {trained && (
+              <CardContainer>
+                <AnimateOnReveal>
+                  <CardText>
+                    Answer:
+                    {' '}
+                    {data.cards[currentCard].answer}
+                  </CardText>
+                  {/* Missed It and Got It buttons should connect to the SRS algorithm */}
+                  <ButtonContainer>
+                    <CardButton type="button" onClick={this.handleMissed}>Missed It</CardButton>
+                    <CardButton type="button" onClick={this.handleGot}>Got It</CardButton>
+                    {(currentCard + 1) !== data.cards.length
+                      ? (
+                        <NextCardButton type="button" onClick={this.nextCard} showNext={showNext}>Next</NextCardButton>
+                      )
+                      : (
+                        // Routing users back to deckl ist for now. Could add intermediary
+                        // modal with further options (e.g. train again, deck list, dashboard, etc)
+                        // showNext to string is recommended fix to console warning
+                        <NextCardLink to="/dashboard/decks" shownext={showNext.toString()}>End Session</NextCardLink>
+                      )
+                    }
+                  </ButtonContainer>
+                </AnimateOnReveal>
+                <NextCardProgressText hidePrompt={showNext}>
+                  How did you do? Select to see the next card.
+                </NextCardProgressText>
+              </CardContainer>
+            )}
+            {!trained && (
+              <CardInteractionsAnswer>
+                <CardButton type="button" onClick={this.showAnswer}>Show Answer</CardButton>
+              </CardInteractionsAnswer>
+            )}
+            <ProgressText>
+              Progress:
+              {currentCard}
+              /
+              {data.cards.length}
+            </ProgressText>
+            {/* Toggles more options for UX: edit card, quit current training, etc */}
+            {/* Could be modal? */}
+            <OptionsButton type="button" title="Options" onClick={this.toggleOption}>...</OptionsButton>
+            <OptionsMenu status={showOptions}>
+              <OptionItem
+                onClick={this.quitTrainingSession}
+              >
+                Quit current training session.
+              </OptionItem>
+            </OptionsMenu>
+          </CardModal>
+        </MainCardContainer>
       ) : null
     );
   }
@@ -124,35 +128,81 @@ class Card extends React.Component {
 export default Card;
 
 // styles
+
 const CardContainer = styled.div`
 `;
 
-const CardData = styled.div`
+// covers and darkens viewport except for modal
+const MainCardContainer = styled(CardContainer)`
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
 `;
 
+const CardModal = styled.div`
+  background-color: #43525c;
+  width: 600px;
+  margin: 232px auto;
+  padding: 2%;
+`;
+
+
 const CardTitle = styled.h2`
+padding-bottom: 2%;
+border-bottom: 1px solid white;
 `;
 
 const CardText = styled.p`
 `;
 
-const CardInteractions = styled.div`
+const CardInteractionsAnswer = styled(CardContainer)`
+  text-align: center;
+`;
+
+const ButtonContainer = styled(CardContainer)`
+  display: flex;
+  justify-content: space-between;
+  width: 65%;
+  margin: 0 auto;
 `;
 
 const CardButton = styled.button`
+  height: 35px;
+  width: 125px;
 `;
 
-const NextCardButton = styled.button`
+const NextCardButton = styled(CardButton)`
   display: ${props => (props.showNext ? 'inline-block' : 'none')};
 `;
 
 // because prop value is cast to a string, the condition will evaluate to true when value is false
 const NextCardLink = styled(Link)` 
-  display: ${props => (props.shownext === 'true' ? 'inline-block' : 'none')};
-  font-size: 16px;
+  display: ${props => (props.shownext === 'true' ? 'flex' : 'none')};
+  height: 35px;
+  width: 125px;
+  font-size: 15px;
+  border-radius: 3px;
+  margin-top: 5px;
+  color: white;
+  background: lightseagreen;
+  border: 1px solid white;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box; /* this needs to be on everything! */
+
+    &:hover {
+      text-decoration: none;
+    }
 `;
 
-const ProgressText = styled.p`
+const ProgressText = styled(CardText)`
+  text-align: center;
   font-size: 12px;
 `;
 
@@ -161,7 +211,8 @@ const NextCardProgressText = styled(ProgressText)`
   font-size: 12px;
 `;
 
-const OptionsButton = styled.div`
+const OptionsButton = styled(CardContainer)`
+  text-align: end;
   cursor: pointer;
           
   &:hover {
@@ -169,11 +220,14 @@ const OptionsButton = styled.div`
   }
 `;
 
-const OptionsMenu = styled.div`
+const OptionsMenu = styled(CardContainer)`
   display: ${props => (props.status ? 'block' : 'none')};
+  margin-top: 2%;
+  border-top: 1px solid white;
 `;
 
-const OptionItem = styled.p`
+const OptionItem = styled(CardText)`
+  text-align: end;
   cursor: pointer;
         
   &:hover {
@@ -181,6 +235,7 @@ const OptionItem = styled.p`
   }
 `;
 
+// animation styling
 const FadeIn = keyframes`
   0% {
     opacity: 0;
@@ -191,10 +246,11 @@ const FadeIn = keyframes`
   }
 `;
 
-const AnimateOnReveal = styled.div`
+const AnimateOnReveal = styled(CardContainer)`
   animation: ${FadeIn} 2s;
 `;
 
+// prop checking
 Card.defaultProps = {
   data: null,
 };
