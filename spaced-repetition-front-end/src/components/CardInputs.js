7cabe71a276fef29657ad68abd9b112cf450217b
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-11-29 13:32:30 -0800

    add deck feature complete, can add cards in it

--- spaced-repetition-front-end/src/components/CardInputs.js
+++ spaced-repetition-front-end/src/components/CardInputs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 class CardInputs extends React.Component {
   constructor(props) {
@@ -9,6 +10,8 @@ class CardInputs extends React.Component {
       question: '',
       answer: '',
       language: '',
+      deck_id: '',
+      saved: false,
     };
   }
 
@@ -30,25 +33,56 @@ class CardInputs extends React.Component {
   saveCard = (e) => {
     e.preventDefault();
     const { state, props } = this;
-    props.onCardSave(state);
+    const { title, question, answer, language } = state;
+    props.onCardSave({ title, question, answer, language });
+
+    //disable save button to stop resubmission of card. FIX: should change this to be editable 
+    this.setState({ saved: true })
   }
 
   render() {
-    const { state } = this;
+    const { state, props } = this;
     return (
-      <div>
-        <h2>Add New Card:</h2>
-        <form onSubmit={this.saveCard}>
+      <Container>
+        {this.props.cardNumber === 1 && <h2>Add New Card:</h2>}
+        <AddCardForm onSubmit={this.saveCard}>
           <input type="text" value={state.title} name="title" onChange={this.handleChange} placeholder="Title" required />
           <textarea type="text" value={state.question} name="question" onChange={this.handleChange} placeholder="Question" required />
           <textarea type="text" value={state.answer} name="answer" onChange={this.handleChange} placeholder="Answer" required />
-          <input type="number" value={state.deck_id} name="deck_id" onChange={this.handleChange} placeholder="deck_id" required />
+          {/* <input type="number" value={state.deck_id} name="deck_id" onChange={this.handleChange} placeholder="deck_id" required /> */}
           <input type="text" value={state.language} name="language" onChange={this.handleChange} placeholder="Language" required />
-          <button type="submit">Save</button>
-        </form>
-      </div>
+          {!state.saved && <button type="submit">Save</button>}
+        </AddCardForm>
+      </Container>
     );
   }
 }
 
 export default CardInputs;
+
+// styles
+const Container = styled.div`
+  padding-top: 20px;
+`
+
+const AddCardForm = styled.form`
+  padding: 40px;
+  margin: 5px;
+  /* width: 50%; */
+  border: 1px solid ${props => props.theme.dark.sidebar};
+  background: ${props => props.theme.dark.cardBackground};
+  border-radius: 4px;
+  height: 325px;
+`;
+
+const PublicDiv = styled.div`
+  display: flex;
+  width: 40%;
+  justify-content: space-between;
+  align-items: center;
+`
+const PublicBox = styled.input`
+  width: 20px;
+  margin-top: 8px;
+  border-radius: 2px;
+`
