421cdb7f0be5227ec348352c6bb79fd1f2907da6
gooseandmegander <meganawilliamson@gmail.com>
2018-11-29 23:55:27 -0800

    Cardlist displays cards from all decks

--- spaced-repetition-front-end/src/components/CardList.js
+++ spaced-repetition-front-end/src/components/CardList.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import CardView from './CardView';
 
-const CardList = () => {
-  return <div>Hello Cardlist</div>;
+const CardList = ({ decks }) => {
+  let cards = [];
+  decks.map((deck) => {
+    cards = cards.concat(deck.cards);
+  });
+
+  return (
+    <div>
+      {cards.map(card => <p>card.title</p>)}
+    </div>
+  );
 };
 
 export default CardList;
+
+CardList.propTypes = {
+  decks: PropTypes.instanceOf(Array).isRequired,
+};
---
+++ spaced-repetition-front-end/src/components/CardView.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Card from './Card';
+
+const CardView = (props) => {
+  return <div>cards!!!</div>;
+};
+
+export default CardView;
