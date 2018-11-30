b68a97c2b1a083fcfa5b8fc8210559e7b4f7cde2
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-11-30 13:09:53 -0800

    init

--- spaced-repetition-front-end/src/components/CardView.js
+++ spaced-repetition-front-end/src/components/CardView.js
@@ -2,26 +2,59 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const CardView = ({ card, deckName }) => {
-  console.log('card', card);
-  const { tags } = card;
-  // const tags = ['js', 'css', 'plaintext'];
-  return (
-    <Card>
-      <p>{`Title: ${card.title}`}</p>
-      <p>{`Question: ${card.question}`}</p>
-      <p>{`Answer: ${card.answer}`}</p>
-      <p>{`Language: ${card.language}`}</p>
-      <p>Tags:</p>
-      <TagsContainer>
-        {tags && tags.map(tag => <p key={tag}>{tag}</p>)}
-      </TagsContainer>
-      <CardInteractions>
-        <p>{`From deck: ${deckName}`}</p>
-        <button type="button">Edit</button>
-      </CardInteractions>
-    </Card>
-  );
+class CardView extends React.Component {
+  state = {
+    dropDownOpenDecks: false,
+    dropDownOpenLangs: false,
+    languages: ['JavaScript', 'CSS', 'none'],
+    deckNames: [],
+    selectedLang: 'none',
+    selectedDeck: 'none',
+    deckId: 0,
+    title: '',
+    question: '',
+    answer: '',
+    language: '',
+    tags: '',
+  };
+
+
+  componentDidMount = () => {
+    const { grabDeckInfo } = this.props;
+    // const deckNames = grabDeckInfo();
+    // this.setState({ deckNames }, () => console.log('deckNames', deckNames));
+  }
+
+
+  editMode = () => {
+    console.log('edit')
+  }
+
+
+  render() {
+
+
+    const { card, deckName } = this.props
+    // const { tags } = card;
+    const tags = ['js', 'css', 'plaintext'];
+
+    return (
+      <Card>
+        <p>{`Title: ${card.title}`}</p>
+        <p>{`Question: ${card.question}`}</p>
+        <p>{`Answer: ${card.answer}`}</p>
+        <p>{`Language: ${card.language}`}</p>
+        <p>Tags:</p>
+        <TagsContainer>
+          {tags && tags.map(tag => <p key={tag}>{tag}</p>)}
+        </TagsContainer>
+        <CardInteractions>
+          <p>{`From deck: ${deckName}`}</p>
+          <button type="button" onClick={this.editMode}>Edit</button>
+        </CardInteractions>
+      </Card>
+    );
+  }
 };
 
 export default CardView;
@@ -44,7 +77,7 @@ const TagsContainer = styled.div`
   display: flex;
 
   p {
-    border: 1px solid black;
+    /* border: 1px solid black; */
     padding: 2%;
     margin: 2%;
   }
