ca8901a8fed28e782284aff4458273bdfe205f31
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-12-07 12:16:48 -0800

    revert changes

--- spaced-repetition-front-end/src/components/LandingPage/LandingPage.js
+++ spaced-repetition-front-end/src/components/LandingPage/LandingPage.js
@@ -25,7 +25,7 @@ const LandingPage = ({ auth }) => {
             <p>Studying with us is as easy as creating your own digital flashcards and decks. We take care of the rest! Are you a programmer? Check out our code snippet integration!</p>
           </div>
           <CTAButtonsGroup>
-            {auth.isAuthenticated() ? <CTABtn onClick={login}>Dashboard</CTABtn> : <CTABtn onClick={login}>Sign up</CTABtn>}
+            <CTABtn onClick={login}>Sign up</CTABtn>
             <CTABtn learn href="#why">Learn more</CTABtn>
           </CTAButtonsGroup>
         </JumboTron>
@@ -52,130 +52,129 @@ export default LandingPage;
 // styles
 
 const WrapperContainer = styled.div`
-    height: 100%;
-    padding-top: 55px;
+height: 100%;
+padding-top: 55px;
 // margin-top: ${props => props.isLoggedIn ? '90px' : '55px'};
-      
+
 // @media (max-width: 540px) {
-        //   h1 {
-        //     font-size: 28px;
-        //   }
-        // }
-        `;
+//   h1 {
+//     font-size: 28px;
+//   }
+// }
+`;
 
 const Header = styled.div`
 height: 100%;
-      `;
+`;
 
 const JumboTron = styled.div`
-      height: 60%;
-      width: 100%;
-      padding: 5% 8%;
-        background-size: cover;
-        background-position: center bottom;
-        letter-spacing: 1px;
-        color: white;
-        // display: flex;
-        // flex-direction: column;
-        // justify-content: space-between;
-      
+height: 60%;
+width: 100%;
+padding: 5% 8%;
+  background-size: cover;
+  background-position: center bottom;
+  letter-spacing: 1px;
+  color: white;
+  // display: flex;
+  // flex-direction: column;
+  // justify-content: space-between;
+
   &::before {
-        background - image: url(${headerImg});
-      background-size: cover;
-      content: "";
-      display: block;
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 60%;
-      z-index: -2;
-      opacity: 0.2;
-      }
-  
+    background-image: url(${headerImg});
+    background-size: cover;
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 60%;
+    z-index: -2;
+    opacity: 0.2;
+    }
+
     // div {
-        //   max-width: 700px;
-        //   width: 100%;
-        // }
+    //   max-width: 700px;
+    //   width: 100%;
+    // }
 
-        // h1 {
-        //   font-size: 38px;
-        // }
+    // h1 {
+    //   font-size: 38px;
+    // }
 
-        // p {
-        //   margin-top: 30px;
-        //   line-height: 25px;
-        // }
-        `;
+    // p {
+    //   margin-top: 30px;
+    //   line-height: 25px;
+    // }
+`;
 
 const Body = styled.div`
 height: 100%;
-        margin-top: 55px;
-        position: relative;
-        flex:1;
-        overflow:hidden;
-        scroll-behavior: smooth;
-        -webkit-overflow-scrolling:touch;
-      `;
+  margin-top: 55px;
+  position: relative;
+  flex:1;
+  overflow:hidden;
+  scroll-behavior: smooth;
+  -webkit-overflow-scrolling:touch;
+`;
 
 const CTAButtonsGroup = styled.div`
-      text-align: center;
-      margin: 0 auto;
-      
-button {
-        // width: 200px;
+text-align: center;
+margin: 0 auto;
 
-      }
-      `;
+button {
+  // width: 200px;
+  
+}
+`;
 
 const CTABtn = styled.button`
-${props => props.theme.dark.buttons.base}
-      font-size: 18px;
-      height: 40px;
+font-size: 18px;
+height: 40px;
 margin-right: ${props => props.learn ? 0 : '20px'};
 color: ${props => props.learn ? null : 'lightseagreen'};
 border-color: ${props => props.learn ? null : 'lightseagreen'};
 background: ${props => props.learn ? null : 'none'};
-      width: 200px;
-      
+width: 200px;
+
 @media (max-width: 540px) {
-        width: 115px;
-    }
-    `;
+  width: 115px;
+}
+`;
 
 const ArrowContainer = styled.div`
-    position: absolute;
-    top: 95%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    `;
+position: absolute;
+top: 95%;
+left: 50%;
+transform: translate(-50%, -50%);
+`;
 
 const BackToTopContainer = styled.div`
-    position: absolute;
-    top: 95%;
-    right: 5%;
-    `;
+position: absolute;
+top: 95%;
+right: 5%;
+`;
 
 const animate = keyframes`
 0% {
-        opacity: 0;
-    }
+  opacity: 0;
+}
 50% {
-        opacity: 1;
-    }
+  opacity: 1;
+}
 100% {
-        opacity: 0;
-    }
-      `;
+  opacity: 0;
+}
+  `;
 
 const Arrow = styled.span`
-    display: block;
-    width: 20px;
-    height: 20px;
-    border-color: black; // working in dev tools - not here
+display: block;
+width: 20px;
+height: 20px;
+border-color: black; // working in dev tools - not here
 // border-bottom-color: ${props => props.theme.dark.mainFontColor}
-      border-bottom: 1px solid;
-      border-right: 1px solid;
-      transform: rotate(45deg);
+border-bottom: 1px solid;
+border-right: 1px solid;
+transform: rotate(45deg);
 animation: ${animate} 2s infinite;
-      `;
+`;
