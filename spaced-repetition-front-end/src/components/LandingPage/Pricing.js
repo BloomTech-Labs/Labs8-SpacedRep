e2250dac7d48f437c07201499d79f720174d225a
gooseandmegander <meganawilliamson@gmail.com>
2018-12-06 21:44:27 -0800

    mobile responsive pricing page

--- spaced-repetition-front-end/src/components/LandingPage/Pricing.js
+++ spaced-repetition-front-end/src/components/LandingPage/Pricing.js
@@ -7,43 +7,45 @@ const Pricing = () => {
       <Content>
         <h2>Pricing</h2>
         <Boxes>
-          {/* <LeftBox>1</LeftBox> */}
-          {/* <RightBox>2</RightBox> */}
           <LeftBox>
             <PricingCTA>
               <p>
                 Our mission is to give people like you the tools you need to have a successful study session.
-                That is why we are offering all our features and team support to every tier.
-                {/* The only difference you'll find between our free and unlimited tiers is the amount of cards and decks you can create. */}
-                We want you to have the true experience of our service from day one.
+                That is why we are offering all our features and team support to every tier. We want you to have the true experience of our service from day one.
               </p>
               <button type="button">Get Started!</button>
             </PricingCTA>
           </LeftBox>
           <RightBox>
             <CardsContainer>
-              <FreeCard>
-                <Tier>Free</Tier>
-                <Price><span>0</span>/mo.</Price>
-                <Item><i class="fas fa-check fs-2x" />snippet</Item>
-                <Item><i class="fas fa-check fs-2x" />SRS</Item>
-                <Item><i class="fas fa-check fs-2x" />support</Item>
-                <Item><i class="fas fa-check fs-2x" />deck sharing</Item>
-                <Item><i class="fas fa-check fs-2x" />training mode</Item>
-                <Item><i class="fas fa-check fs-2x" /># of decks</Item>
-                <Item><i class="fas fa-check fs-2x" /># of cards</Item>
-              </FreeCard>
-              <UnlimitedCard>
-                <Tier>Unlimited</Tier>
-                <Price><span>9</span>.99/mo.</Price>
-                <Item><i class="fas fa-check fs-2x" />snippet</Item>
-                <Item><i class="fas fa-check fs-2x" />SRS</Item>
-                <Item><i class="fas fa-check fs-2x" />support</Item>
-                <Item><i class="fas fa-check fs-2x" />deck sharing</Item>
-                <Item><i class="fas fa-check fs-2x" />training mode</Item>
-                <Item><i class="fas fa-check fs-2x" /># of decks</Item>
-                <Item><i class="fas fa-check fs-2x" /># of cards</Item>
-              </UnlimitedCard>
+              <FreeContainer>
+                <Header>
+                  <Tier>Free</Tier>
+                  <Price><span>0</span>/mo.</Price>
+                </Header>
+                <Card>
+                  <Item><i class="fas fa-check fs-2x" />Code Snippets</Item>
+                  <Item><i class="fas fa-check fs-2x" />Built-in SRS</Item>
+                  <Item><i class="fas fa-check fs-2x" />Team Support</Item>
+                  <Item><i class="fas fa-check fs-2x" />Deck Sharing</Item>
+                  <Item><i class="fas fa-check fs-2x" />Training Mode</Item>
+                  <Item><i class="fas fa-check fs-2x" />3 Decks/150 Cards</Item>
+                </Card>
+              </FreeContainer>
+              <UnContainer>
+                <Header>
+                  <Tier>Unlimited</Tier>
+                  <Price><span>9</span>.99/mo.</Price>
+                </Header>
+                <Card>
+                  <Item><i class="fas fa-check fs-2x" />Code Snippets</Item>
+                  <Item><i class="fas fa-check fs-2x" />Built-in SRS</Item>
+                  <Item><i class="fas fa-check fs-2x" />Team Support</Item>
+                  <Item><i class="fas fa-check fs-2x" />Deck Sharing</Item>
+                  <Item><i class="fas fa-check fs-2x" />Training Mode</Item>
+                  <Item><i class="fas fa-check fs-2x" /> Unlimited Usage</Item>
+                </Card>
+              </UnContainer>
             </CardsContainer>
           </RightBox>
         </Boxes>
@@ -57,7 +59,6 @@ export default Pricing;
 // styled
 
 const Container = styled.section`
-// border: 1px solid gold; // temp
 width: 100%;
 height: 100%;
 margin: auto 0;
@@ -65,7 +66,6 @@ padding: 5%;
 `;
 
 const Content = styled.div`
-// border: 1px solid gold; // temp
 width: 100%;
 height: 100%;
 
@@ -78,7 +78,6 @@ h2 {
 `;
 
 const Boxes = styled.div`
-// border: 1px solid gold; // temp
 width: 100%;
 height: 90%;
 display: flex;
@@ -86,7 +85,6 @@ justify-content: space-between;
 
 @media (max-width: 900px) {
   flex-direction: column-reverse;
-  // space-between gone?
 }
 `;
 
@@ -118,7 +116,6 @@ const LeftBox = styled.div`
   @media (max-width: 900px) {
     width: 100%;
     height: 55%;
-    // align-items center gone?
 
     button {
       margin-bottom: 0;
@@ -129,7 +126,6 @@ const LeftBox = styled.div`
   `;
 
 const PricingCTA = styled.div`
-  // border: 1px solid gold; // temp
 width: 100%;
 height: 80%;
 display: flex;
@@ -139,10 +135,16 @@ justify-content: space-between;
 @media (max-width: 900px) {
   height: 100%;
 }
+
+@media (max-width: 600px) {
+  p {
+    font-size: 14px;
+    line-height: 1.7;
+  }
+}
 `;
 
 const RightBox = styled.div`
-// border: 1px solid gold; // temp
 width: 60%;
 height: 100%;
 display: flex;
@@ -159,29 +161,77 @@ display: flex;
 justify-content: space-around;
 align-items: center;
 
-// space-around and center gone ?
 @media (max-width: 900px) {
   align-items: flex-start
 }
+
+@media (max-width: 600px) {
+  flex-direction: column;
+}
 `;
 
-const FreeCard = styled.ul`
+const FreeContainer = styled.div`
 border: 1px solid gray;
 background-color: #3c4f5d;
 width: 45%;
 height: 90%;
 box-shadow: 6px 6px 15px 1px black;
+
+@media (max-width: 600px) {
+  width: 100%;
+  height: 40%;
+  box-shadow: none;
+}
 `;
 
-const UnlimitedCard = styled(FreeCard)`
+const UnContainer = styled(FreeContainer)`
 width: 40%;
 height: 80%;
 box-shadow: none;
 `;
 
+const Header = styled.div`
+width: 100%;
+height: 30%;
+
+span {
+    font-size: 50px;
+    
+    &::before {
+      content: '$';
+      position: absolute;
+      font-size: 15px;
+      margin-left: -10px;
+      margin-top: 10px;
+    }
+  }
+
+  @media (max-width: 600px) {
+    display: flex;
+    justify-content: space-between;
+    height: 40%;
+    background: lightseagreen;
+    padding: 0 4%;
+  }
+}
+`;
+
+const Card = styled.ul`
+height: 70%;
+
+@media (max-width: 600px) {
+  display: flex;
+  height: 60%;
+  display: flex;
+  height: 60%;
+  flex-wrap: wrap;
+  justify-content: center;
+  font-size: 14px;
+}
+`;
+
 const Item = styled.li`
-// height: 10.625%;
-height: 8.125%;
+height: 16.666%;
 text-align: left;
 padding-top: 5%;
 
@@ -192,22 +242,32 @@ i {
 @media (max-width: 900px) {
   padding-top: 2%;
 }
+
+@media (max-width: 600px) {
+  // margin: 0px 2%;
+  width: 48%;
+}
 `;
 
-const Tier = styled(Item)`
-height: 15%;
+const Tier = styled.p`
+padding-top: 4%;
+height: 40%;
 font-size: 32px;
 font-weight: bold;
 text-align: center;
 background: lightseagreen;
             
 @media (max-width: 900px) {
-padding-top: 4%;
-height: 18%;
+padding-top: 1%;
+font-size: 30px;
+}
+
+@media (max-width: 600px) {
+  padding-top: 4%;
 }
 `;
 
-const Price = styled(Item)`
+const Price = styled.p`
 height: 20%;
 padding-top: 8%;
 text-shadow: 1px 1px 2px black;
@@ -226,101 +286,6 @@ span {
 }
 
 @media (max-width: 900px) {
-  padding-top: 2%;
+  padding-top: 1%;
 }
 `;
-
-// const Container = styled.section`
-// align-items: center;
-// // justify-content: space-between;
-// height: 100%;
-// width: 100%;
-// padding: 50px;
-// display: flex;
-
-// :first-child {
-//   width: 300px;
-//   height: 100%;
-// }
-
-// h1 {
-//   font-size: 36px;
-// }
-// `;
-
-// const SubscriptionsContainer = styled.div`
-// justify-content: space-between;
-// display: flex;
-// width: 100%;
-// `;
-
-// const FreeTierBox = styled.ul`
-// max-height: 400px;
-// height: 100%;
-// max-width: 300px;
-// width: 100%;
-// background-color: #299488;
-
-// li {
-//   padding: 20px;
-// }
-// `;
-
-// const PaidTierBox = styled(FreeTierBox)`
-// `;
-
-// const Item = styled.li`
-// `;
-
-// const TierItem = styled.li`
-// background-color: #24312f;
-// font-size: 30px;
-// text-shadow: 1px 1px 5px black;
-// padding: 20px;
-// background-color: darkslategray;
-// `;
-
-// const PriceItem = styled.li`
-// text-align: center;
-// span {
-//   font-size: 50px;
-
-//   &::before {
-//     content: '$';
-//     position: absolute;
-//     font-size: 15px;
-//     margin-left: -10px;
-//     margin-top: 10px;
-//   }
-// }
-// `;
-
-
-{/* <SubscriptionsContainer >
-  <div>
-    <h1>Pricing</h1>
-    <p><a>Click here</a> to get started with a free or paid account and begin creating your own flashcards!</p>
-  </div>
-  <FreeTierBox>
-    <TierItem>Free</TierItem>
-    <PriceItem>
-      <span>0</span>
-      /mo.
-    </PriceItem>
-    <Item>Code Snippets</Item>
-    <Item>Deck sharing</Item>
-    <Item>3 Decks</Item>
-    <Item>150 Cards</Item>
-  </FreeTierBox>
-  <PaidTierBox>
-    <TierItem>Paid</TierItem>
-    <PriceItem>
-      <span>9</span>
-      .99/mo.
-    </PriceItem>
-    <Item>Code Snippets</Item>
-    <Item>Deck sharing</Item>
-    <Item>Unlimited Decks</Item>
-    <Item>Unlimited Cards</Item>
-  </PaidTierBox>
-</SubscriptionsContainer> */}
