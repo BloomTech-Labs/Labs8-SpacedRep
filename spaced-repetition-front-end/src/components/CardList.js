d04c316f4d4d143b8e901009bce4121671681435
Gabriel Duquette <gabrielduquette@gmail.com>
2018-12-10 12:29:48 -0500

    limit cards for free users

--- spaced-repetition-front-end/src/components/CardList.js
+++ spaced-repetition-front-end/src/components/CardList.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Modal from 'react-modal';
 import Card from './Card';
 import CardListTools from './CardListTools';
 // import CardInputs from './CardInputs';
 import AddCard from './AddCard';
 
-
 //FIX: if 1 or 2 cards, the card's height is way too much and looks terrible
 class CardList extends Component {
   state = {
     addNewCard: false,
-    // deckArr: [],
+    modalIsOpen: false,
   };
 
   componentDidMount = () => {
@@ -20,14 +20,24 @@ class CardList extends Component {
 
 
   handleAddCard = () => {
-    const { decks, history } = this.props;
-    if (decks.length === 0) history.push('/dashboard/decks')
-    this.setState({ addNewCard: !this.state.addNewCard });
+    const { profile, decks, history } = this.props;
+    if (decks.length === 0) history.push('/dashboard/decks');
+    let cardCount;
+    for (let i = 0; i < decks.length; i++) {
+      cardCount += decks[i].cards.length;
+    }
+    if (profile.tier === 'free' && cardCount >= 150) {
+      this.setState({ modalIsOpen: true });
+      return;
+    }
+    this.setState(prevState => ({
+      addNewCard: !prevState.addNewCard,
+    }));
   }
 
   handleDeckData = () => {
     const { decks } = this.props;
-    const deckData = decks.map(deck => {
+    const deckData = decks.map((deck) => {
       return { id: deck.id, name: deck.name };
     })
     console.log('deckData from cardlist', deckData);
@@ -39,11 +49,26 @@ class CardList extends Component {
     history.push('/dashboard/decks')
   }
 
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  }
+
   render() {
     const { decks } = this.props;
-    const { addNewCard, deckArr } = this.state;
+    const { addNewCard, modalIsOpen } = this.state;
     return (
       <CardListContainer id="CardListContainer">
+        <ModalWrapper isOpen={modalIsOpen} onRequestClose={this.closeModal}>
+          <Text>Only paid users can make more than 150 cards!</Text>
+          <ButtonContainer>
+            <SaveButton onClick={this.goToCheckout} type="submit">
+              Go to checkout
+            </SaveButton>
+            <Cancel onClick={this.closeModal} type="submit">
+              No thanks
+            </Cancel>
+          </ButtonContainer>
+        </ModalWrapper>
         <CardListTools addNewCard={this.handleAddCard} />
         {addNewCard && <AddCard grabDeckInfo={this.handleDeckData} toggleAddCard={this.handleAddCard} />}
         {/* <div id="cardlistcontainer"> */}
@@ -57,7 +82,7 @@ class CardList extends Component {
 
         {decks.length === 0 && !addNewCard && (
           <Welcome>
-            <h3>Hey, it doesn't look like you haven't made any cards or decks yet!</h3>
+            <h3>{"Hey, it looks like you haven't made any cards or decks yet!"}</h3>
             <p>Click <span onClick={this.handleWelcomeClick}> Decks </span>on the sidebar to get started.</p>
           </Welcome>
         )}
@@ -114,9 +139,67 @@ const Welcome = styled.div`
     cursor:pointer;
   }
   }
-`
+`;
 
+const ModalWrapper = styled(Modal)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  transform: translate(130%, 100%);
+  padding: 25px;
+  width: 350px;
+  height: 200px;
+  border: 1px solid black;
+  color: white;
+  background: ${props => props.theme.dark.cardBackground};
+  border-radius: 4px;
+  &:focus {
+    outline: none;
+  }
+  @media (max-width: 500px) {
+    transform: translate(7%, 60%);
+  }
+`;
+
+const Text = styled.p`
+  width: 300px;
+  font-size: 25px;
+  text-align: center;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SaveButton = styled.button`
+  ${props => props.theme.dark.buttons.base}
+  &:hover {
+    background: ${props => props.theme.dark.logo};
+    cursor: pointer;
+  }
+  font-size: 16px;
+`;
+
+const Cancel = styled.button`
+  border: none;
+  background: none;
+  color: lightgrey;
+  font-weight: bold;
+  font-size: 20px;
+  height: 26px;
+  margin: 0px;
+  padding: 10px 0 0 0;
+  color: white;
+  &:hover {
+    text-decoration: underline;
+  }
+  /* width: 100px; */
+`;
 
 CardList.propTypes = {
-  decks: PropTypes.instanceOf(Object).isRequired,
+  decks: PropTypes.instanceOf(Array).isRequired,
+  profile: PropTypes.instanceOf(Object).isRequired,
+  history: PropTypes.instanceOf(Object).isRequired,
 };
